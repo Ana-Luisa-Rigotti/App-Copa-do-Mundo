d20c1c66590cf8d59dc57ac99da1e14dc1405bca
Ana-Luisa-Rigotti <alohapovo@gmail.com>
2026-05-13 22:20:23 -0300

    primeira tabela

--- app/src/main/java/com/example/app_copa_do_mundo/MainActivity.java
+++ app/src/main/java/com/example/app_copa_do_mundo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.app_copa_do_mundo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        LinearLayout btnTabelas = findViewById(R.id.btn_tabelas);
+
+        btnTabelas.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, tabelas.class);
+            startActivity(intent);
+        });
     }
+
 }
